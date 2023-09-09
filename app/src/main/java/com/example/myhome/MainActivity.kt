464734be4e0a868cf910cfc2c8a65126a5f6f105
amirbek-ashirbek <amirbek.ashirbek@gmail.com>
2023-09-09 21:09:36 +0600

    Redesigned colors of system bars

--- app/src/main/java/com/example/myhome/MainActivity.kt
+++ app/src/main/java/com/example/myhome/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.myhome.navigation.MyHomeNavHost
@@ -22,10 +22,10 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			MyHomeTheme {
 				val navController = rememberNavController()
-				Box(
+				Surface(
 					modifier = Modifier
 						.fillMaxSize()
-						.systemBarsPadding()
+						.navigationBarsPadding()
 				) {
 					MyHomeNavHost(
 						navController = navController
