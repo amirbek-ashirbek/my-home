3a31811ccca16e1479e0ed2f00d4d8c5d069a200
amirbek-ashirbek <amirbek.ashirbek@gmail.com>
2023-09-11 01:58:04 +0600

    design/ Changed ripple effect size and shape on lock icon

--- app/src/main/java/com/example/myhome/feature_home/presentation/my_home_screen/components/door/DoorItem.kt
+++ app/src/main/java/com/example/myhome/feature_home/presentation/my_home_screen/components/door/DoorItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -123,6 +125,8 @@ fun DoorItem(
 				if (isFromDatabase) {
 					Box(
 						modifier = Modifier
+							.size(32.dp)
+							.clip(RoundedCornerShape(12.dp))
 							.clickable(
 								onClick = onLockClicked
 							)
@@ -130,12 +134,16 @@ fun DoorItem(
 						if (isLocked) {
 							Image(
 								painter = painterResource(id = R.drawable.icon_lock),
-								contentDescription = "Lock icon"
+								contentDescription = "Lock icon",
+								modifier = Modifier
+									.align(Alignment.Center)
 							)
 						} else {
 							Image(
 								painter = painterResource(id = R.drawable.icon_lock_unlocked),
-								contentDescription = "Lock icon"
+								contentDescription = "Lock icon",
+								modifier = Modifier
+									.align(Alignment.Center)
 							)
 						}
 					}
